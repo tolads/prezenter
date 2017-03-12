938c58fce9be454026f06157035757d0de9d1d5a
Tamas Laszlo <tlaszlods@gmail.com>
2017-03-12 09:48:03 +0100

    Delete reports after deleting presentation

--- api/models/Presentations.js
+++ api/models/Presentations.js
@@ -25,4 +25,25 @@ module.exports = {
     },
   },
 
+  /**
+   * Called after destroying Presentation records, delete their stuff also
+   * @param  {Object[]} deletedRecords
+   * @param  {Function} cb - callback function
+   */
+  afterDestroy: (deletedRecords, cb) => {
+    const deleteReports = deletedRecords.map(presentation => (
+      new Promise((resolve, reject) => {
+        Reports.destroy({
+          presentation: presentation.id,
+        })
+          .then(resolve)
+          .catch(reject);
+      })
+    ));
+
+    Promise.all(deleteReports)
+      .then(() => cb())
+      .catch(() => cb());
+  },
+
 };
